180972e10ea48b08d774cc4433ee96312e511cc7
unknown <danielsah118@gmail.com>
2024-08-11 00:54:31 +0000

    modified UI

--- src/Components/Dashboard/Coaching/Complains.jsx
+++ src/Components/Dashboard/Coaching/Complains.jsx
@@ -1,4 +1,5 @@
 //import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { complainsObj } from "./complainsObj";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
