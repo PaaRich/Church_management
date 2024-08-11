c3674e48393cc9b7f3bd0bfbfb4a6ebce9cdd9b2
PaaRich <siehrichmond50@gmail.com>
2024-08-11 09:53:07 +0000

    gist

--- src/Components/Dashboard/Coaching/Complains.jsx
+++ src/Components/Dashboard/Coaching/Complains.jsx
@@ -1,6 +1,5 @@
-
-import { complainsObj } from "./complainsObj";
-import { Link } from "react-router-dom";
+//import { complainsObj } from "./complainsObj";
+//import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { getAllComplaints } from "../../../Redux/features/complaint/complaintSlice";
@@ -8,6 +7,7 @@ import Loader from "../../Reusable/Loader";
 
 const Complains = () => {
   const dispatch = useDispatch();
+  // eslint-disable-next-line no-unused-vars
   const { complaintLoading, complaintSuccess, allComplaints } = useSelector(
     (state) => state.complaint
   );
@@ -30,30 +30,32 @@ const Complains = () => {
 
   return (
     <>
-    {complaintLoading && <Loader/>}
-    <div>
-      <div className="mt-5">
-        <h1 className="font-semibold text-2xl">Complains</h1>
-        <div className="w-full mt-5">
-          <table className="w-full border-2 border-black c-complains">
-            <thead className="bg-blue-500 text-white border-b-4">
-              <tr>
-                <td className="border-r-2  font-semibold text-xl py-3 w-1">
-                  No.
-                </td>
-                <td className="border-r-2 font-semibold text-xl py-3">
-                  Date Created
-                </td>
-                <td className="border-r-2  font-semibold text-xl py-3">
-                  Contacts
-                </td>
-                <td className="font-semibold text-xl py-3 ">Complaint Type</td>
-              </tr>
-            </thead>
-            <tbody>
-              {complaints?.map((complain, index) => (
-                <tr key={index} className="duration-500">
-                  {/* <Link
+      {complaintLoading && <Loader />}
+      <div>
+        <div className="mt-5">
+          <h1 className="font-semibold text-2xl">Complains</h1>
+          <div className="w-full mt-5">
+            <table className="w-full border-2 border-black c-complains">
+              <thead className="bg-blue-500 text-white border-b-4">
+                <tr>
+                  <td className="border-r-2  font-semibold text-xl py-3 w-1">
+                    No.
+                  </td>
+                  <td className="border-r-2 font-semibold text-xl py-3">
+                    Date Created
+                  </td>
+                  <td className="border-r-2  font-semibold text-xl py-3">
+                    Contacts
+                  </td>
+                  <td className="font-semibold text-xl py-3 ">
+                    Complaint Type
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                {complaints?.map((complain, index) => (
+                  <tr key={index} className="duration-500">
+                    {/* <Link
                     to={`/dashboard/coaching/${complain.contact}`}
                     state={complain}
                     className="w-full block"
@@ -65,30 +67,36 @@ const Complains = () => {
                     <td className="border-r-2">{complain.type}</td>
                     <td>{complain.date}</td>
                   </Link> */}
-                  <td className="border-r-2">
-                    <span>{`${index + 1}.`}</span>
-                  </td>
-                  <td>{new Date(complain.createdAt).toLocaleString("en-US",{month:"short",day:"2-digit",year:"numeric"})}</td>
-                  <td className="border-r-2 ">{complain?.active_number}</td>
-                  <td className="border-r-2">{complain.complaint_type}</td>
-                </tr>
-              ))}
-              {/* <Link>
+                    <td className="border-r-2">
+                      <span>{`${index + 1}.`}</span>
+                    </td>
+                    <td>
+                      {new Date(complain.createdAt).toLocaleString("en-US", {
+                        month: "short",
+                        day: "2-digit",
+                        year: "numeric",
+                      })}
+                    </td>
+                    <td className="border-r-2 ">{complain?.active_number}</td>
+                    <td className="border-r-2">{complain.complaint_type}</td>
+                  </tr>
+                ))}
+                {/* <Link>
                 <tr>
                   <td className="w-full">d</td>
                   <td className="w-full">d</td>
                   <td className="w-full">d</td>
                 </tr>
               </Link> */}
-            </tbody>
-          </table>
-          {/* <table>
+              </tbody>
+            </table>
+            {/* <table>
               <thead></thead>
               <tbody></tbody>
             </table> */}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
